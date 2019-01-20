b17044d2ddac5e7924bfe9092bb7394d64e0db0d
Peter Vilhan <vilhan@vnet.eu>
2019-01-20 14:16:13 +0100

    pre release verzia aj s uploadom

--- index.php
+++ index.php
@@ -4,32 +4,34 @@
 
   require_once 'library.php';
 //upload file
-  $target_dir = "input/";
-  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
-  if(isset($_POST["submit"])) {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-       echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-   } else {
-       echo "Sorry, there was an error uploading your file.";
-   }
-  //exract files
-  process_zip($target_dir . basename($_FILES["fileToUpload"]["name"]));
+if (isset($_POST["submit"])) {
+    $target_dir = "input/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $filetype = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if ($filetype == "zip") {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.<br />";
+        } else {
+            die("Sorry, there was an error uploading your file to {$target_file}.");
+        }
+        //exract files
+        if (extract_zip($target_file)) {
+            //process file in loop and delete after processing
+            if (process_fa($target_dir)) {
+                if (is_file($target_file)) {
+                    //delete archive at the end
+                    unlink($target_file);
+                }
+            }
+        }
 
-//process file in loop and delete after processing
-
-
-//delete archive at the end
-
-/*
-    openfile(__DIR__ . '/input/Fa_k_DPH/F182345 Motosam.xls');
-    openfile(__DIR__ . '/input/Fa_k_DPH/F182402 Subtil pril 1.xls');
-    openfile(__DIR__ . '/input/Fa_k_DPH/F182402 Subtil pril 2.xls');
-*/
-}
-else {
-  ?>
+    } else {
+        echo "Pokusili ste sa nahrat nepovoleny typ suboru. Nahravajte len ZIP archiv.";
+    }
+  
+} else {
+    ?>
 <form action="index.php" method="post" enctype="multipart/form-data">
     Subor s archivom faktur:&nbsp;
     <input type="file" name="fileToUpload" id="fileToUpload">
--- library.php
+++ library.php
@@ -12,23 +12,22 @@ class MyReadFilter implements IReadFilter
     public $columns_computed = ['S','T'];
     public function readCell($column, $row, $worksheetName = '')
     {
-        if ($row == 8 and $column == 'D'){
+        if ($row == 8 and $column == 'D') {
             return true;
-        }
-        else if ($row >= 19 && $row <= 46) {
+        } elseif ($row >= 19 && $row <= 46) {
             if (in_array($column, $this->columns)) {
-            return true;
-          }
-        }
-        else {
-          return false;
+                return true;
+            }
+        } else {
+            return false;
         }
     }
 }
 
-function openfile($path){
+function openfile($path)
+{
     $inputFileName = $path;
-    echo('Loading file ' . pathinfo($inputFileName, PATHINFO_BASENAME) . '<br />');
+    echo('Spracuvam subor: ' . pathinfo($inputFileName, PATHINFO_BASENAME) . '<br />');
 
     try {
         $sheetname = 'List_zákazníka';
@@ -40,8 +39,6 @@ function openfile($path){
         $spreadsheet = $reader->load($inputFileName);
         $worksheet = $spreadsheet->getActiveSheet();
 
-
-        //$sheetData = $spreadsheet->getActiveSheet()->toArray(0, true, false, false);
         $faktura = $spreadsheet->getActiveSheet()->getCell('D8')->getValue();
 
 
@@ -49,48 +46,66 @@ function openfile($path){
         $delenie = 0;
         $totals = array();
         $last_pcn = 0;
-        for($i=18;$i<=46;$i++){
-          //spocitam sumu za delenie
-          if(!strcasecmp(trim($worksheet->getCell('B'.$i)->getValue()),"delenie mat.")){
-            $delenie += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(),2);
-          }
-            $new_pcn = trim($worksheet->getCell('C'.$i)->getValue());
-            if($new_pcn == "" && !strcasecmp(trim($worksheet->getCell('B'.$i)->getValue()),"delenie mat.") && $last_pcn != 0){
-              $totals[$last_pcn]['sum'] += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(),2);
+        for ($i=18;$i<=46;$i++) {
+            //spocitam sumu za delenie
+            if (!strcasecmp(trim($worksheet->getCell('B'.$i)->getValue()), "delenie mat.")) {
+                $delenie += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(), 2);
             }
-            else {
-              $totals[$new_pcn]['sum'] += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(),2);
-              $totals[$new_pcn]['mnozstvo'] += round($worksheet->getCell('S'.$i)->getOldCalculatedValue(),2);
+            $new_pcn = trim($worksheet->getCell('C'.$i)->getValue());
+            if ($new_pcn == "" && !strcasecmp(trim($worksheet->getCell('B'.$i)->getValue()), "delenie mat.") && $last_pcn != 0) {
+                $totals[$last_pcn]['sum'] += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(), 2);
+            } else {
+                $totals[$new_pcn]['sum'] += round($worksheet->getCell('T'.$i)->getOldCalculatedValue(), 2);
+                $totals[$new_pcn]['mnozstvo'] += round($worksheet->getCell('S'.$i)->getOldCalculatedValue(), 2);
             }
             $last_pcn = trim($worksheet->getCell('C'.$i)->getValue());
-
         }
         echo "<br />Faktura: ".$faktura;
         echo "<br />Suma za delenie: ".$delenie;
         echo "<br /><br />Statistiky:<br />";
-        foreach($totals as $kat=>$val){
-          if($kat>0){
-            echo 'Kategoria:'.$kat.'<br />';
-            echo 'Sum: '.$val['sum'].'<br />';
-            echo 'Mnozstvo: '.$val['mnozstvo'].'<br /><br />';
-          }
+        foreach ($totals as $kat=>$val) {
+            if ($kat>0) {
+                echo 'Kategoria:'.$kat.'<br />';
+                echo 'Sum: '.$val['sum'].'<br />';
+                echo 'Mnozstvo: '.$val['mnozstvo'].'<br /><br />';
+            }
         }
         echo '<hr><br />';
-
+        return true;
     } catch (InvalidArgumentException $e) {
-
         echo('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . $e->getMessage());
+        return false;
     }
 }
 
-function process_zip($fullpath){
+function extract_zip($fullpath)
+{
+    $zip = new ZipArchive;
+    if ($zip->open($fullpath) === true) {
+        $zip->extractTo('input/');
+        $zip->close();
+        echo 'Subory boli uspesne extrahovane<br />';
+        return true;
+    } else {
+        die('Cannot open file'.$fullpath.' Zip extract failed');
+    }
+}
 
-  $zip = new ZipArchive;
-  if ($zip->open($fullpath) === TRUE) {
-      $zip->extractTo('input/');
-      $zip->close();
-      echo 'ok';
-  } else {
-      echo 'failed';
-  }
+function process_fa($dir)
+{
+    if ($handle = opendir($dir)) {
+        while (false !== ($entry = readdir($handle))) {
+            $filetype = strtolower(pathinfo($dir.$entry, PATHINFO_EXTENSION));
+            // Check if image file is a actual image or fake image
+            if ($filetype == "xls") {
+                if (openfile($dir . $entry)) {
+                    if (is_file($dir . $entry)) {
+                        unlink($dir . $entry);
+                    }
+                }
+            }
+        }
+        closedir($handle);
+        return true;
+    }
 }
